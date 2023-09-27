146909e708b0709eaebed9f7cd8a0b2714fcf003
Junaid Ali <113332138+JunaidAly@users.noreply.github.com>
2023-09-27 17:58:26 +0500

    Add files via upload

---
+++ Lab4Task/Lab4Task/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Lexer
+{
+    private StreamReader reader;
+    private char currentChar;
+    private StringBuilder buffer1 = new StringBuilder();
+    private StringBuilder buffer2 = new StringBuilder();
+    private bool usingBuffer1 = true;
+
+    public Lexer(string filePath)
+    {
+        reader = new StreamReader(filePath);
+        Advance();
+    }
+
+    // Switch between buffers
+    private void SwitchBuffers()
+    {
+        if (usingBuffer1)
+        {
+            usingBuffer1 = false;
+            buffer1.Clear();
+        }
+        else
+        {
+            usingBuffer1 = true;
+            buffer2.Clear();
+        }
+    }
+
+    // Get the current character
+    private char Peek()
+    {
+        return currentChar;
+    }
+
+    // Consume the current character and advance to the next one
+    private void Advance()
+    {
+        int nextChar = reader.Read();
+        currentChar = (nextChar == -1) ? '\0' : (char)nextChar;
+    }
+
+    // Add the current character to the current buffer
+    private void AddToBuffer(char c)
+    {
+        if (usingBuffer1)
+            buffer1.Append(c);
+        else
+            buffer2.Append(c);
+    }
+
+    // Get the content of the active buffer
+    private string GetActiveBuffer()
+    {
+        return usingBuffer1 ? buffer1.ToString() : buffer2.ToString();
+    }
+
+    // Lexical analysis logic
+    public Token GetNextToken()
+    {
+        while (true)
+        {
+            if (Peek() == '\0') // End of input
+            {
+                reader.Close();
+                return new Token(TokenType.EOF, "");
+            }
+            else if (Char.IsWhiteSpace(Peek()))
+            {
+                // Skip whitespace characters
+                Advance();
+            }
+            else if (Char.IsLetter(Peek()))
+            {
+                // Handle identifiers
+                while (Char.IsLetterOrDigit(Peek()))
+                {
+                    AddToBuffer(Peek());
+                    Advance();
+                }
+                return new Token(TokenType.Identifier, GetActiveBuffer());
+            }
+            else
+            {
+                // Handle other tokens (operators, symbols, etc.)
+                AddToBuffer(Peek());
+                Advance();
+                return new Token(TokenType.Unknown, GetActiveBuffer());
+            }
+        }
+    }
+}
+
+enum TokenType
+{
+    EOF,
+    Identifier,
+    Unknown
+}
+
+class Token
+{
+    public TokenType Type { get; private set; }
+    public string Value { get; private set; }
+
+    public Token(TokenType type, string value)
+    {
+        Type = type;
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{Type}: {Value}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "sample.txt"; // Replace with the path to your source code file
+        Lexer lexer = new Lexer(filePath);
+
+        Token token;
+        do
+        {
+            token = lexer.GetNextToken();
+            Console.WriteLine(token);
+        } while (token.Type != TokenType.EOF);
+    }
+}
---
+++ LabActivity5/LabActivity5/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Symbol
+{
+    public string Name { get; }
+    public string DataType { get; }
+    // Add other attributes as needed
+
+    public Symbol(string name, string dataType)
+    {
+        Name = name;
+        DataType = dataType;
+    }
+}
+
+public class SymbolTable
+{
+    private const int TableSize = 100; // Adjust the table size as needed
+    private List<Symbol>[] table;
+
+    public SymbolTable()
+    {
+        table = new List<Symbol>[TableSize];
+        for (int i = 0; i < TableSize; i++)
+        {
+            table[i] = new List<Symbol>();
+        }
+    }
+
+    // Define a hash function to map symbol names to table indices
+    private int Hash(string name)
+    {
+        int hash = 0;
+        foreach (char c in name)
+        {
+            hash += (int)c;
+        }
+        return hash % TableSize;
+    }
+
+    // Add a symbol to the symbol table
+    public void AddSymbol(Symbol symbol)
+    {
+        int index = Hash(symbol.Name);
+        table[index].Add(symbol);
+    }
+
+    // Retrieve a symbol from the symbol table by name
+    public Symbol GetSymbol(string name)
+    {
+        int index = Hash(name);
+        foreach (Symbol symbol in table[index])
+        {
+            if (symbol.Name == name)
+            {
+                return symbol;
+            }
+        }
+        return null; // Symbol not found
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SymbolTable symbolTable = new SymbolTable();
+
+        // Add symbols to the symbol table
+        symbolTable.AddSymbol(new Symbol("x", "int"));
+        symbolTable.AddSymbol(new Symbol("y", "float"));
+        symbolTable.AddSymbol(new Symbol("z", "string"));
+
+        // Retrieve symbols from the symbol table
+        Symbol symbolX = symbolTable.GetSymbol("x");
+        if (symbolX != null)
+        {
+            Console.WriteLine($"Symbol Name: {symbolX.Name}, Data Type: {symbolX.DataType}");
+        }
+        else
+        {
+            Console.WriteLine("Symbol not found.");
+        }
+    }
+}
+
